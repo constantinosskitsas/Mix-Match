1a766ebfe01f4d700f8fb23f78efd5f9549ed837
constantinosskitsas <constantinosskitsas@hotmail.com>
2025-09-03 11:11:38 +0200

    test_change1

--- SubgraphMatchingSurvey-main/vlabel/build/matching/createData.py
+++ SubgraphMatchingSurvey-main/vlabel/build/matching/createData.py
@@ -3,19 +3,23 @@ import numpy as np
 algo=["_FT0","_FT1","_FT2","_FT3","_FT4","_FT5"]
 algo=["DV","FT1_DV","FT2_DV","FT3_DV","FT4_DV","FT5_DV","LFTJ","RM","VEQ"]
 timts=["1","10","50","100","1000"]
-#timts=["10"]
+timts=["10"]
 timtsn=[1,10,50,100,1000]
-#timtsn=[10]
+timtsn=[10]
+timsts=["cycle","path","tall","tall_wide","wide_flat","flat_narrow"]
+
+timts=timsts
 algo=["DV","FT1_DV","FT2_DV","FT3_DV","FT4_DV","LFTJ","RM","VEQ"]
 #algo=["_DVGM","FT1_DVGM","FT2_DVGM","FT3_DVGM","FT4_DVGM","_LFSK","_LFTJK"]
-algo=["DLSBS"]
+algo=["MMN","MMI","VEQ","RM","DSQL","DSQLP","LFTJK"]
+
 
 #timtsn=[1]
 #FMPath="RE_PL_-1_patents_G16"
 FMPath="performance_experiment/NSCORRLFTJ_LFTJ_-1_dblp_G_8"
 FMPath2="performance_experiment/Skase_VEQ_-1_dblp_G_8"
 FMPath="performance_experiment/ADIVERSITY_F0__LFTJ_-1_dblp_G_8"
-FMPath="performance_experiment/DIV-BASIC_"
+FMPath="../../build/matching/performance_experiment/SHUTUP_"
 
 #FMPath="RE_PL_-1_patents_G"
 pathsize=["8","16","24","32","64"]
@@ -50,19 +54,21 @@ AMC7=[]
 print(np.shape(pathsize))
 TOPK=["10","50","100","500","1000","2500","5000","10000"]
 TOPK_N=["10","50","100","500","1000","2500","5000","10000"]
-
+my_set = set()
  #with open(f'{folderall_}/{at}/{i}/fusbal_Tresults{t}.txt', 'r') as file:
 for i in range(len(algo)):
 #for i in range(len(pathsize)):
     for j in range(len(timts)):
         counter=0
         #with open(f'{FMPath}_{timts[j]}{algo[i]}{endf}', 'r') as file:
-        with open(f'{FMPath}{algo[i]}_-1_youtube_G_64_{timts[j]}{endf}', 'r') as file:
+        #with open(f'{FMPath}{algo[i]}_-1_youtube_G_64_{timts[j]}{endf}', 'r') as file:
+        with open(f'{FMPath}{algo[i]}_-1_patents_{timts[j]}_16_50{endf}', 'r') as file:
             for line in file:
                 values = line.split()
                 if(counter<10000):
                     MC.append(float(values[4]))
                     counter=counter+1
+                    my_set.add(values[7])
                 MC1.append(float(values[11]))
                 MC2.append(float(values[12]))
                 MC3.append(float(values[13]))
@@ -81,9 +87,10 @@ for i in range(len(algo)):
         #avg_UN[i][j]=np.mean(UN)
        # avg_TC[i][j]=np.mean(TC)
         #avg_TP[i][j]=np.mean(TP)
-        if(len(MC)!=100 and len(MC)!=200):
-            print("and so we pray",len(MC))
-            print(algo[i],timts[j])
+        if(len(MC)!=20):
+            print("not 100")
+        #if(len(MC)!=200):
+        #    print("and so we pray",len(MC))
         MC.clear()
         MC1.clear()
         MC2.clear()
@@ -94,7 +101,7 @@ for i in range(len(algo)):
         MC7.clear()
 
 print(FMPath)
-
+#print(len(my_set))
 
 
 #for i in range(len(algo)):
@@ -113,7 +120,7 @@ print(FMPath)
 
 i=0
 print("Time ALG ALG10 ALG50 ALG100 ALG250 ALG500 ALG750 ALG1000")
-print("TOPK F2DVGM F4DVGM DVGMSQ LFSK LFTJK")
+print(algo)
 for j in range(len(timts)): 
     for t in range(1):   
     #for t in range(len(TOPK_N)):
